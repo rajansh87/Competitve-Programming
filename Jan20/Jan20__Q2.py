abed357f1033885f77ad3a65c1ee3a21fbfee956
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-01-17 20:53:42 +0530

    Add files via upload

---
+++ Jan20/Jan20__Q2.py
@@ -0,0 +1,20 @@
+t=int(input())
+for ww in range(t):
+    s=0
+    n=int(input())
+    a=int(input())
+    s=(2*(10**n)+a)
+    print(s)
+    b=int(input())
+    c=(10**n)-b
+    print(c)
+    d=int(input())
+    e=((10**n)-d)
+ 
+    print(e)
+    
+    ans=int(input())
+    if(ans==(-1)):
+        break
+ 
+
