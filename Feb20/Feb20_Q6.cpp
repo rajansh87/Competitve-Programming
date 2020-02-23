84e28085f91f12c36adf99a82a6a91fb2c3ba936
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-02-24 00:01:23 +0530

    Add files via upload

---
+++ Feb20/Feb20_Q6.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    while(c--)
+    {
+        int x,y;
+        cin>>x>>y;
+    
+    }
+cout<<1<<" "<<1<<" "<<0<<endl;
+
+
+return 0;
+}
