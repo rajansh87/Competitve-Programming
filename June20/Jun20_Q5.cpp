346d557cecd66a0f6594362aeac895d27c84f7f8
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-06-21 18:49:01 +0530

    Add files via upload

---
+++ June20/Jun20_Q5.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define loop1 for(i=0;i<n;i++)
+#define loop2 for(j=0;j<n;j++)
+#define loopi(a,b) for(i=a;i<b;i++)
+#define loopj(a,b) for(j=a;j<b;j++)
+#define rloopi(a,b) for(i=a;i>b;i--)
+#define rloopj(a,b) for(j=a;j>b;j--)
+#define test long long int t;cin>>t;while(t--)
+#define loopvar(var,a,b) for(var=a;var<b;var++)
+#define rloopvar(var,a,b) for(var=a;var>b;var--)
+bool checkPrime(ll n)
+{
+    if(n<=1){return false;}
+    if(n<=3){return true;}
+    if(n%2==0 || n%3==0){return false;}
+    for(ll i=5;i*i<=n;i=i+6){if(n%i==0 || n%(i+2)==0){return false;}}
+    return true;
+}
+ll i,j;
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll arr[n][n];
+    ll z=0;
+    loop1
+    {
+        loop2
+        {
+            arr[i][j]=++z;
+        }
+    }
+    if(n%2==0)
+        {
+            ll x=0,y=0,z=0,p=0;
+            loopi(0,(ll)n/2)
+            {
+                y=i+1;
+                z=y*n-1;
+                p=arr[y][z];
+                loop2
+                {
+                    arr[y][x++]=p--;
+                }
+            }
+        }
+    loop1
+    {
+        loop2
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    test
+    {
+        solve();
+    }
+
+    /*ll testcase=1;
+    test
+    {
+        cout<<"Case #"<<testcase<<": ";
+        solve();
+        cout<<endl;
+
+        testcase++;
+    }*/
+    return 0;
+}
