adba88bec6e948a146220313d43262d2305a154c
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-12-30 01:15:48 +0530

    Update Longest Common Prefix.py

--- LeetCode/Longest Common Prefix.py
+++ LeetCode/Longest Common Prefix.py
@@ -4,8 +4,6 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if len(strs)==0:
             return ""
-        if len(strs)==1:
-            return strs[0]
         if strs==[strs[0]]*len(strs):
             return strs[0]
         strs.sort(reverse=True)
